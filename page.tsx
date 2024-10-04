051226e0204c8a7e494a854cb9e73d355c24abd4
Sajjad Lashari <khansahab0289@gmail.com>
2024-10-04 09:33:20 +0500

    next js first assignment

---
+++ page.tsx
@@ -0,0 +1,23 @@
+// import Image from "next/image";
+
+
+    export default function Home() {
+      return (
+        <>
+          
+    
+          {/* Below Code Is With Tailwind */}
+          <div className="flex justify-between items-center">
+            <div className="w-fit h-[250px] m-[40px] text-[80px] font-black">
+              Hey,
+              <br />
+
+              <div className="name">My Name is</div>
+              <span className="pinkColor">SAJJAD</span>
+              <br />I am Next JS Developer
+            </div>
+            <div className=""></div>
+          </div>
+        </>
+      );
+    }
